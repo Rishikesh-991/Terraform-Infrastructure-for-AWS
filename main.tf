8d93f0327a940a8241a4a20a5135a778b524ce07
Rishikesh991 <rishikeshkourav991@gmail.com>
2026-02-04 12:13:29 +0000

    complete

--- main.tf
+++ main.tf
@@ -4,5 +4,5 @@ terraform {
 
 # This file is a lightweight placeholder so running `terraform plan` from
 # /home/ubuntu/AWS-terraform does not error with "No configuration files".
-# Do NOT run applies here. Use the environment folders under
+# Do NOT run applies here.
 # terraform-aws-infra/environments (dev/stage/prod) for real work.
