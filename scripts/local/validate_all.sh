7571d04dd29116f93c98d4820a0c1c9ab7e394ef
Rishikesh-991 <94486135+Rishikesh-991@users.noreply.github.com>
2026-02-13 01:49:20 +0530

    Add validation for Terraform script

--- scripts/local/validate_all.sh
+++ scripts/local/validate_all.sh
@@ -2,9 +2,12 @@
 set -euo pipefail
 ROOT_DIR="$(cd "$(dirname "$0")/../.." && pwd)/terraform-aws-infra"
 for env in dev stage prod; do
+
   echo "== Validating $env =="
   pushd "$ROOT_DIR/environments/$env" >/dev/null
   terraform init -backend=false
+  
   terraform validate
+  
   popd >/dev/null
 done
